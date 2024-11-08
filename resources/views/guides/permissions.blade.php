58ae9082ac727e15b92ce06abc800d004f6a7c38
victoralmeidamag <victorhugo.almeidamag@gmail.com>
2024-11-07 23:37:39 -0300

    Criando permissao de perfil

--- resources/views/guides/permissions.blade.php
+++ resources/views/guides/permissions.blade.php
@@ -10,7 +10,12 @@
                 <tr>
                     <th>ID</th>
                     <th>Nome</th>
-                    <th>Administrador</th>
+                    @if(Auth::user()->is_master)
+                        <th>Administrador</th>
+                    @endif
+                    @if(Auth::user()->is_admin || Auth::user()->is_master)
+                        <th>Perfil</th>
+                    @endif
                 </tr>
             </thead>
             <tbody>
@@ -18,15 +23,26 @@
                     <tr>
                         <td>{{ $user->id }}</td>
                         <td>{{ $userService->treatmentName($user->name) }}</td>
+                        @if(Auth::user()->is_master)
                         <td>
                             <form method="POST" action="{{ route('update-user-admin', ['user_id' => $user->id, 'is_admin' => $user->is_admin]) }}">
                                 @csrf
                                 <input type="checkbox" class="admin-checkbox" data-user-id="{{ $user->id }}" 
                                        {{ $user->is_admin ? 'checked' : '' }} 
                                        onchange="this.form.submit()"
-                                       @disabled(!Auth::user()->is_master)>
+                                       >
                             </form>
-                        </td>                    
+                        </td>
+                        @endif
+                        @if(Auth::user()->is_admin || Auth::user()->is_master)
+                            <td>
+                                <select name="" id="">
+                                    <option value="1">Oi</option>
+                                    <option value="2">xau</option>
+                                    <option value="3">Ei</option>
+                                </select>
+                            </td>
+                        @endif           
                     </tr>
                 @endforeach
             </tbody>
